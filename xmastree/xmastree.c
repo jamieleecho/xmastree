1a389a4878fd2fb93afe2cc3dc4876217af81c82
Jamie Cho <jamieleecho@gmail.com>
2025-12-07 23:24:44 -0800

    Setup the window

--- xmastree/xmastree.c
+++ xmastree/xmastree.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <cgfx.h>
+
+#define NULL ((void *)0)
+#define WT_FWIN 2
+
+
+static int sigcode;
+void sighandler(int sig) {
+    sigcode = sig;
+}
+
+
+WNDSCR mywindow = {
+    "xmastree",     /* window title */
+    0,              /* number of menus */
+    40,             /* min. window width */
+    24,             /* min. window height */
+    0xC0C0,       	/* sync bytes */
+    {0,0,0,0,0,0,0},/* reserved */
+    NULL           	/* pointer to menu descriptors */
+};
+
 
 int main(int argc, char **argv) {
-    printf("Hello, Xmas Tree!\n");
+    intercept(sighandler);
+    int err = _cgfx_ss_wnset(0, WT_FWIN, &mywindow);
+
     return 0;
 }
